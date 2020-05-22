457f387a47f636515dc3d25cc8dda88258e721b1
Jan Andrin Ghadamian <ghjan@student.ethz.ch>
2020-05-22 11:18:17 +0200

    main menu sczene und main menu skript

---
+++ Assets/scripts/MainMenuScrpt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScrpt : MonoBehaviour
+{
+    
+    public void startGame (string scene) {
+
+        SceneManager.LoadScene(scene);
+
+    }
+
+    public void exitGame() {
+
+
+        Application.Quit ();
+    }
+}
